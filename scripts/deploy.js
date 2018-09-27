1bf495b9191b9bc6e1d74a72f0f0f507cd61d916
Shawn Xie <fengluo17@gmail.com>
2018-09-27 11:16:23 +0800

    fix: catch getAccountSync error for newaccountSync

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,24 +5,27 @@ const fibosClient = FIBOS(config.client);
 const fs = require('fs');
 
 // create account
-const user = fibosClient.getAccountSync('todo');
-if (!(user.account_name && user.account_name === 'todo')){
-    fibosClient.newaccountSync({
-        creator: 'eosio',
-        name: "todo",
-        owner: config.account.publicKey,
-        active: config.account.publicKey
-    });
+try {
+  const user = fibosClient.getAccountSync('todo');
+  console.log('user: ', user);
+} catch (error) {
+  fibosClient.newaccountSync({
+    creator: 'eosio',
+    name: 'todo',
+    owner: config.account.publicKey,
+    active: config.account.publicKey,
+  });
+  console.log('create account successfully');
 }
 
-//setcode
-const js_code = fs.readTextFile('contracts/todo.js');
-fibosClient.setcodeSync(config.contract.name, 0, 0, fibosClient.compileCode(js_code));
+// setcode
+const jsCode = fs.readTextFile('contracts/todo.js');
+fibosClient.setcodeSync(config.contract.name, 0, 0, fibosClient.compileCode(jsCode));
 
-//getcode
+// getcode
 const code = fibosClient.getCodeSync(config.contract.name, true);
 console.log('code:', code);
 
-//setabi
+// setabi
 const abi = JSON.parse(fs.readTextFile('contracts/todo.abi'));
 fibosClient.setabiSync(config.contract.name, abi);
